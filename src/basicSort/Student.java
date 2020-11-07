ca3d51a2f7f205602a47ca40b227ac2b076be9e3
陈会琼 <chenhuiqiong@chenhuiqiongdeMacBook-Pro.local>
2020-11-07 14:58:51 +0800

    Revert "Revert "init""
    This reverts commit 1ed41a7cd04ca0a76bfb2ce8d799f51ff2be1781.

---
+++ src/basicSort/Student.java
@@ -0,0 +1,29 @@
+package basicSort;
+
+public class Student implements Comparable<Student> {
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public int compareTo(Student another) {
+        if (this.score < another.score) {
+            return -1;
+        } else if (this.score == this.score) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    @Override
+    public String toString() {
+
+//        return String.format("basicSearch.Student(%s, score: %d)", this.name, this.score);
+        return String.format("%d", this.score);
+    }
+}
