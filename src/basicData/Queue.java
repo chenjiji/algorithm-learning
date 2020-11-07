1ed41a7cd04ca0a76bfb2ce8d799f51ff2be1781
陈会琼 <chenhuiqiong@chenhuiqiongdeMacBook-Pro.local>
2020-11-07 14:57:29 +0800

    Revert "init"
    This reverts commit e7eab7a46f31aa2d5502e4fe45e3708b500aa8d7.

--- src/basicData/Queue.java
+++
@@ -1,9 +0,0 @@
-package basicData;
-
-public interface Queue<E extends Comparable<E>>  {
-    public void enqueue(E e);
-    public E dequeue();
-    public E getFront();
-    public int getSize();
-    public boolean isEmpty();
-}
