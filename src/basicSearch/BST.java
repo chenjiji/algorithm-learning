c96fe27f7aab31dc83ea179227224188129f6793
chenji <541918762@qq.com>
2021-09-07 15:47:35 +0800

    完成二分搜索树

--- src/basicSearch/BST.java
+++ src/basicSearch/BST.java
@@ -1,8 +1,6 @@
 package basicSearch;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class BST<E extends Comparable<E>> {
     private class Node {
@@ -26,6 +24,10 @@ public class BST<E extends Comparable<E>> {
 
     public int size() {
         return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
     }
 
     public Node getRoot() {
@@ -88,6 +90,10 @@ public class BST<E extends Comparable<E>> {
     // 递归-打印
     @Override
     public String toString() {
+        if (this.size() == 0) {
+            throw new IllegalArgumentException("BST is Empty.");
+        }
+
         StringBuilder str = toString(new StringBuilder(), this.getRoot());
 
         return "打印输出: " + str.toString();
@@ -122,7 +128,7 @@ public class BST<E extends Comparable<E>> {
 
     // 非递归-前序遍历
     public void preOrder2() {
-        Stack<Node> stack = new Stack();
+        Stack<Node> stack = new Stack<Node>();
         stack.push(this.getRoot());
         System.out.print("前序遍历: ");
 
@@ -171,7 +177,7 @@ public class BST<E extends Comparable<E>> {
 
     // 层序遍历
     public void levelOrder() {
-        Queue<Node> queue = new LinkedList<>();
+        Queue<Node> queue = new LinkedList<Node>();
         queue.add(this.getRoot());
         System.out.print("层序遍历: ");
 
@@ -190,27 +196,131 @@ public class BST<E extends Comparable<E>> {
 
     // 递归-判断值是否存在
     public boolean contain(E e) {
-        return contain(this.getRoot(), e);
+        return contain(this.getRoot(), e) != null;
     }
 
-    public boolean contain(Node root, E e) {
+    public Node contain(Node root, E e) {
         if (root != null) {
             if (root.e.compareTo(e) == 0)
-                return true;
+                return root;
             else if (root.e.compareTo(e) > 0)
                 return contain(root.left, e);
             else
                 return contain(root.right, e);
         }
 
-        return false;
+        return null;
+    }
+
+    public E maximum() {
+        return maximum(this.getRoot()).e;
+    }
+
+    private Node maximum(Node node) {
+        if (this.size() == 0) {
+            throw new IllegalArgumentException("BST is Empty.");
+        }
+
+        if (node.right != null)
+            return maximum(node.right);
+        else
+            return node;
+    }
+
+    public E minimum() {
+        return minimum(this.getRoot()).e;
+    }
+
+    private Node minimum(Node node) {
+        if (this.size() == 0) {
+            throw new IllegalArgumentException("BST is Empty.");
+        }
+
+        if (node.left != null)
+            return minimum(node.left);
+        else
+            return node;
+    }
+
+    public E delMaximum() {
+        if (this.size() == 0) {
+            throw new IllegalArgumentException("BST is Empty.");
+        }
+
+        Node root = this.getRoot();
+
+        return delMaximum(root, root).e;
+    }
+
+    private Node delMaximum(Node parent, Node current) {
+        if (current.right != null)
+            return delMaximum(current, current.right);
+
+        parent.right = current.left;
+        this.size--;
+
+        return current;
+    }
+
+    public E delMinimum() {
+        if (this.size() == 0) {
+            throw new IllegalArgumentException("BST is Empty.");
+        }
+
+        Node root = this.getRoot();
+
+        return delMinimum(root, root).e;
+    }
+
+    private Node delMinimum(Node parent, Node current) {
+        if (current.left != null)
+            return delMinimum(current, current.left);
+
+        parent.left = current.right;
+        this.size--;
+
+        return current;
+    }
+
+    public void remove(E e) {
+        this.root = remove(this.getRoot(), e);
+    }
+
+    private Node remove(Node current, E e) {
+        if (e.compareTo(current.e) > 0)
+            current.right = remove(current.right, e);
+        else if (e.compareTo(current.e) < 0)
+            current.left = remove(current.left, e);
+        else {
+            if (current.right == null && current.left == null)
+                current = null;
+            else if (current.right != null && current.left != null) {
+                Node right = current.right;
+                Node node = delMinimum(right, right);
+
+                size++;
+
+                if (node.e == right.e) {
+                    node.left = current.left;
+                    current = node;
+                } else
+                    current.e = node.e;
+            } else if (current.left != null)
+                current = current.left;
+            else
+                current = current.right;
+
+            size--;
+        }
+
+        return current;
     }
 
     public static void main(String[] args) {
-        BST<Integer> bst = new BST<>();
-        int[] arr = new int[]{5, 3, 6, 8, 4, 2};
+        BST<Integer> bst = new BST<Integer>();
+        Integer[] arr = new Integer[]{5, 3, 6, 8, 4, 2};
 
-        for (int i : arr) {
+        for (Integer i : arr) {
             bst.add(i);
 //            bst.add2(i);
         }
@@ -224,6 +334,35 @@ public class BST<E extends Comparable<E>> {
 //        System.out.println(" ");
 //        System.out.println(bst);
 //        bst.preOrder2();
-        bst.levelOrder();
+//        bst.levelOrder();
+//        System.out.println(bst.minimum());
+//        System.out.println(bst.maximum());
+//        System.out.println(bst);
+//        System.out.println("\t-> " + bst.delMinimum());
+//        System.out.println(bst);
+//        System.out.println("\t-> " + bst.delMinimum());
+
+//        int n = 1000;
+//        Random random = new Random();
+//
+//        for (int i = 0; i < n; i++)
+//            bst.add(random.nextInt(10000));
+//
+//        ArrayList<Integer> nums = new ArrayList<>();
+//
+//        while (bst.size() > 0)
+//            nums.add(bst.delMinimum());
+//
+//        for (int i = 1; i < nums.size(); i++)
+//            if (nums.get(i) < nums.get(i - 1))
+//                throw new IllegalArgumentException("Error");
+//
+//        System.out.println("delMinimum test complete.");
+        System.out.println(bst);
+        for (Integer i : new Integer[]{2, 3, 4, 5, 6, 8}) {
+//        for (Integer i : new Integer[]{6}) {
+            bst.remove(i);
+            System.out.println(bst);
+        }
     }
 }
